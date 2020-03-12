b35f1d1e0e546b38e059658ba9fe08d829c74f51
klavikula <robertonavarrete@outlook.com>
2020-03-12 16:22:05 -0300

    function to permit save a new zombie if any field of victim comes in blank

--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -1,4 +1,4 @@
 class Zombie < ApplicationRecord
     has_many :victims
-    accepts_nested_attributes_for :victims
+    accepts_nested_attributes_for :victims, allow_destroy: true, reject_if: ->(attributes) {attributes['nick'].blank?}
 end

44e860f939553fb56c096730dfa9b9592d4d0157
klavikula <robertonavarrete@outlook.com>
2020-03-13 11:33:39 -0300

    linking Add Victim button to actually do a new text_field and refactorizing

--- app/helpers/zombies_helper.rb
+++ app/helpers/zombies_helper.rb
@@ -1,10 +1,13 @@
 module ZombiesHelper
     def add_victim_button(form)
+        victim_fields = form.fields_for(:victims, Victim.new) do |victims_form|
+            render 'victims/victims_fields', victims_form: victims_form
+        end
         html_options = {
             css: 'btn btn-success',
             href: '#',
             id: 'add-victim',
-            data: {victim:'<li>Soy una banana</li>'}
+            data: {victim: victim_fields}
         }
         content_tag(:button, '+ Add Victim', html_options)
     end

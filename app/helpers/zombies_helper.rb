a78d928e2a4f4ad2cf5236f8358211ac8dbd6285
klavikula <robertonavarrete@outlook.com>
2020-03-13 10:54:00 -0300

    editing the zombies_helper method add_victim_button

--- app/helpers/zombies_helper.rb
+++ app/helpers/zombies_helper.rb
@@ -1,5 +1,11 @@
 module ZombiesHelper
     def add_victim_button(form)
-        content_tag(:button, '+ Add Victim')
+        html_options = {
+            css: 'btn btn-success',
+            href: '#',
+            id: 'add-victim',
+            data: {victim: '<p>Soy una banana</p>'}
+        }
+        content_tag(:button, '+ Add Victim', html_options)
     end
 end
